03c629322b375c8656f45407e20eba75d293b0c5
akshayasripathi08-debug <akshayasripathi08@gmail.com>
2026-04-16 11:00:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const API = 'http://localhost:5000/api';
+const API = '/api';
 
 async function classify() {
   const text = document.getElementById("email").value;
+
   const res = await fetch(API + "/classify", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({text})
+    body: JSON.stringify({ text })
   });
+
   const data = await res.json();
+
   document.getElementById("result").innerText =
     data.is_spam ? "🚫 Spam" : "✅ Not Spam";
 }
